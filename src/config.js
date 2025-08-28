0986c10337edec4bad0fbe03d5f99a31db3495e8
GAN Shengzhe <gansz2004@outlook.com>
2025-08-28 11:59:50 -0400

    logo

--- src/config.js
+++ src/config.js
@@ -9,7 +9,8 @@ import HeroLight from "./images/hero-light.jpg";
 import HeroDark from "./images/hero-dark.jpg";
 
 // Projects Images (add your images to the images directory and import below)
-import Logo from "./images/logo.svg";
+// import Logo from "./images/logo.svg";
+import Logo from "./images/icon.JPG"
 
 /* START HERE
  **************************************************************
